a4fbe6c6deed6d45dd2a7ff0b57114575bb8ba94
RUGMJ7443 <matthewrhysjones@icloud.com>
2022-09-02 12:04:49 +0100

    Made autodust only work on wool

--- src/main/java/net/rugmj/logicalredstone/mixin/OnPlaceMixin.java
+++ src/main/java/net/rugmj/logicalredstone/mixin/OnPlaceMixin.java
@@ -5,7 +5,6 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.rugmj.logicalredstone.StateManager;
@@ -23,7 +22,7 @@ public class OnPlaceMixin {
 	public void onPlaced(World world, BlockPos pos, @NotNull BlockState state, @Nullable LivingEntity placer, ItemStack itemStack, CallbackInfo info) {
 
 		if (!StateManager.getAutoDustState()) return;
-		if (state.getBlock().getName().contains(Text.of("wool"))) return;
+		if (!state.getBlock().getName().getString().contains("Wool")) return;
 		if (!world.getBlockState(pos.up(1)).isAir()) return;
 		world.setBlockState(pos.up(1), Blocks.REDSTONE_WIRE.getDefaultState());
 	}
